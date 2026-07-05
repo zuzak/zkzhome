dea0f6ec223783a5552ca7abb2a11ffbaa560fee
Douglas Gardner <douglas@chippy.ch>
2026-07-05 13:37:52 +0100

    +

--- routes/home.js
+++ routes/home.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => res.render('index.html'))
+const packagejson = require('../package.json')
+
+router.get('/about', async (req, res) => {
+    res.render('index.html', {
+        dependencies: Object.keys(packagejson.dependencies)
+    })
+})
 
 module.exports = router
--- routes/index.js
+++ routes/index.js
@@ -2,3 +2,5 @@ const app = require('..')
 
 app.use(require('./health.js'))
 app.use(require('./home.js'))
+
+app.get('/', async (req, res) => res.render('template.html'))
