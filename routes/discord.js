857edaf23f55122c6ae4cd8b22724411a53be0dc
Douglas Gardner <douglas@chippy.ch>
2026-07-23 21:25:54 +0100

    Fix error on reading .json() if null

--- routes/discord.js
+++ routes/discord.js
@@ -3,7 +3,8 @@ const router = express.Router();
 
 router.get('/discord', async (req, res) => {
     const guildId = process.env.DISCORD_GUILD_ID || null
-    const guild = guildId ? await fetch(`https://discord.com/api/guilds/${guildId}/widget.json`, {timeout: 5000}) : null
+    if (!guildId) return res.render('discord.html')
+    const guild = await fetch(`https://discord.com/api/guilds/${guildId}/widget.json`, {timeout: 5000})
     res.render('discord.html', {guild: await guild.json()})
 })
 
